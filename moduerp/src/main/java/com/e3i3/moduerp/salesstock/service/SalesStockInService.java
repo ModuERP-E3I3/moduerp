8095c43522eef52e08bc72da38a91517e6514a03
ieunji0116 <136577730+ieunji0116@users.noreply.github.com>
2024-10-03 21:03:52 +0900

    Update salesStockIn Create

--- moduerp/src/main/java/com/e3i3/moduerp/salesstock/service/SalesStockInService.java
+++ moduerp/src/main/java/com/e3i3/moduerp/salesstock/service/SalesStockInService.java
@@ -1,9 +1,16 @@
 package com.e3i3.moduerp.salesstock.service;
 
 import java.util.List;
-
 import com.e3i3.moduerp.salesstock.model.dto.SalesStockInDTO;
 
 public interface SalesStockInService {
     List<SalesStockInDTO> getAllSalesStockIn();
+
+    void insertSalesStockIn(SalesStockInDTO salesStockInDTO);
+
+    SalesStockInDTO getSalesStockInDetails(String itemCode);
+
+    void updateSalesStockIn(SalesStockInDTO salesStockInDTO);
+
+    void deleteSalesStockInByItemCode(String itemCode);
 }
--- moduerp/src/main/java/com/e3i3/moduerp/salesstock/service/SalesStockInServiceImpl.java
+++ moduerp/src/main/java/com/e3i3/moduerp/salesstock/service/SalesStockInServiceImpl.java
@@ -1,8 +1,10 @@
 package com.e3i3.moduerp.salesstock.service;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.e3i3.moduerp.salesstock.model.dao.SalesStockInDAO;
 import com.e3i3.moduerp.salesstock.model.dto.SalesStockInDTO;
@@ -16,5 +18,25 @@ public class SalesStockInServiceImpl implements SalesStockInService {
     @Override
     public List<SalesStockInDTO> getAllSalesStockIn() {
         return salesStockInDAO.getAllSalesStockIn();
+    }
+
+    @Override
+    public void insertSalesStockIn(SalesStockInDTO salesStockInDTO) {
+        salesStockInDAO.insertSalesStockIn(salesStockInDTO);
+    }
+
+    @Override
+    public SalesStockInDTO getSalesStockInDetails(String itemCode) {
+        return salesStockInDAO.selectSalesStockInByItemCode(itemCode);
+    }
+
+    @Override
+    public void updateSalesStockIn(SalesStockInDTO salesStockInDTO) {
+        salesStockInDAO.updateSalesStockIn(salesStockInDTO);
+    }
+
+    @Override
+    public void deleteSalesStockInByItemCode(String itemCode) {
+        salesStockInDAO.deleteSalesStockInByItemCode(itemCode);
     }
 }
