97174f5f076abcc3289cab3b59b6f95115a82253
Entity <168009904+SWCGoing@users.noreply.github.com>
2024-10-08 15:42:41 +0900

    UpdateBuyStock

--- moduerp/src/main/java/com/e3i3/moduerp/item/model/dto/ItemDTO.java
+++ moduerp/src/main/java/com/e3i3/moduerp/item/model/dto/ItemDTO.java
@@ -25,7 +25,6 @@ public class ItemDTO implements Serializable {
 	private Timestamp createdOutAt; // 추가된 필드
 	private Timestamp updateOutAt; // 추가된 필드
 	private String accountName; // 거래처명 buystock에서 사용
-	private String empName; // 직원명 buystock에서 사용
 	private String iDirector;
 	
 	
@@ -35,14 +34,6 @@ public class ItemDTO implements Serializable {
 
 	public void setAccountName(String accountName) {
 		this.accountName = accountName;
-	}
-
-	public String getEmpName() {
-		return empName;
-	}
-
-	public void setEmpName(String empName) {
-		this.empName = empName;
 	}
 
 	public String getItemCode() {
